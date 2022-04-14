fd18c68e8db4aa39aede5a0d1081bd99b46c93d1
seomidia <suporte@seomidia.com.br>
2022-04-14 10:56:31 -0300

    init

---
+++ converteme-checkout.php
@@ -0,0 +1,9 @@
+<?php
+    /*
+    Plugin Name: Converteme Checkout
+    Plugin URI: https://converte.me/plugin/converteme-checkout
+    Description: Descricao provisoria Converteme Checkout.
+    Author: Converte
+    Version: 1.0.0
+    Author URI:
+    */
